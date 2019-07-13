5c843e5fd837f6cdaa3091f448ec99df4e19a8ce
Byrnndi Bou <byrnndibou@gmail.com>
2019-07-13 16:15:15 +0700

    Bou

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var connect = require("../config/database")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'I Khow' });
+var query = "select * from positions";
+connect.query(query, function(error, result){
+res.json(result)
+})
 });
-
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'I Khow You Khow' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('contact', { title: 'I Khow You Khow' });
+  res.render('contact', { title: 'I Khow Khow' });
 });
 module.exports = router;
